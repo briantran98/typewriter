1ed1dbaa10e1574daed3287056773ff31b6841fb
Brian Tran <98brian.tran@gmail.com>
2020-06-03 16:52:07 +0000

    Removed callback function

--- typewriter.js
+++ typewriter.js
@@ -1,22 +1,17 @@
 const sentence = 'hello there from lighthouse labs';
 
-const typeWriter = (cb, sentence) => {
+const typeWriter = (sentence) => {
   let count = 0;
   let interval = 0;
   for (const char of sentence) {
     count += 50;
     setTimeout(() => {
       interval++;
-      cb(char)
+      process.stdout.write(char);
       if (interval === sentence.length) {
         console.log()
       }
     }, count);
   }
 };
-
-const printChar = (char) => {
-  process.stdout.write(char);
-};
-
-typeWriter(printChar, sentence);
+typeWriter(sentence);
